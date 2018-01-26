de14f27b00ea4a629cd7f66f9ee3ded8484bf32f
OriLev <sviatos@gmail.com>
2018-01-26 17:16:05 +0200

    add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    extends: "airbnb",
+    plugins: ["react"],
+    env: {
+      "browser": true,
+    },
+    parser: "babel-eslint",
+    rules: {
+      "no-tabs": "off",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "comma-dangle": ["error", {
+        "arrays": "always-multiline",
+        "objects": "always",
+        "imports": "never",
+        "exports": "never",
+        "functions": "always-multiline"
+      }],
+      "react/jsx-filename-extension": ["error", { "extensions": [".js"] }],
+      "react/jsx-curly-spacing": ["error", "always", { "allowMultiline": true }],
+	  	// enforce spacing inside array brackets
+      'array-bracket-spacing': ['error', 'always'],
+      'react/forbid-prop-types': ["off", { forbid: ['any', 'array', 'object'] }],
+    },
+};
